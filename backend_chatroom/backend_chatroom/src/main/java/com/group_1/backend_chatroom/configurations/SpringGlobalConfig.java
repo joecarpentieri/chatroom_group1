b4a74e3a6b0c5dee3415f70514caa51e819f1288
Aanis Nuur <Aanisn10@gmail.com>
2023-12-11 10:51:43 +0000

    added cors

---
+++ backend_chatroom/backend_chatroom/src/main/java/com/group_1/backend_chatroom/configurations/SpringGlobalConfig.java
@@ -0,0 +1,20 @@
+package com.group_1.backend_chatroom.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringGlobalConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry){
+
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedHeaders("*")
+                    .allowedMethods("*");
+
+
+
+    }
+}
