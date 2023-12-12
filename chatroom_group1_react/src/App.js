06019519897096f920f0d26e93a3fc268ee59a43
Gisele Aisaeva <gisel.aisaeva@gmail.com>
2023-12-12 16:49:30 +0000

    Create a log in page

--- chatroom_group1_react/src/App.js
+++ chatroom_group1_react/src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
 import ChatContainer from './containers/ChatContainer';
+import { Box, Modal, Button } from '@mui/material';
+import { useState } from 'react';
 
 function App() {
+  const[open, setOpen] = useState(true);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <>
       <h1>CHATTERBOX</h1>
       < ChatContainer />
+      < Modal 
+        open= {open}
+        onClose={handleClose}>
+          <Box id="log-in-modal">
+            <button onClick={handleClose} id="log-in-button">Log In</button>
+          </Box>
+        </Modal> 
     </>
   );
 }
+
+
+
+
 
 export default App;
