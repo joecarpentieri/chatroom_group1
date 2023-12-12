acf4e906edff25632bbb68b596635225789ab17b
Aanis Nuur <Aanisn10@gmail.com>
2023-12-12 10:30:23 +0000

    did a dummy test for modal component in ChatComponent

--- chatroom_group1_react/src/components/Chat.js
+++ chatroom_group1_react/src/components/Chat.js
@@ -1,13 +1,34 @@
+import { useState } from "react";
+import { Box, Modal } from "@mui/material";
+// import Box from "@mui/material";
+import Button from "@mui/material";
+
 import MessageList from "./MessageList";
 const Chat = ({chatroom}) => {
+
+
+    const [open, setOpen]= useState(false);
+    const handleOpen = () => setOpen(true)
+    const handleClose = () => setOpen(false)
 
     const listOfMessages = chatroom.messages;
     
     return ( 
 
+
+
+
         <>
-            <h4>{chatroom.name}</h4>
-            <MessageList listOfMessages={listOfMessages} />
+            <button onClick={handleOpen}>{chatroom.name}</button>
+            <Modal
+             open ={open}
+             onClose={handleClose}
+             >
+                <Box>
+            <button onClick={handleClose}>X</button>
+            <MessageList listOfMessages={listOfMessages} /> 
+                </Box>
+               </Modal>
         </>
      );
 }
